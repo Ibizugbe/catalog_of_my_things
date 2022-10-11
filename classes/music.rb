38eeb74e4a76d67c0be15ee541f83c43cdfea715
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-11 13:58:08 +0100

    initialuze name, genre, published date and spotify

--- classes/music.rb
+++ classes/music.rb
@@ -1,6 +1,11 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-
+  def initialize(name, genre, publish_date, on_spotify)
+    @name = name
+    @genre = genre
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
 
 end
