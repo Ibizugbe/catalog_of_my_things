e8cd367c632e6b3e396062858704b571c5b7c8aa
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-11 15:37:26 +0100

    create genre class

---
+++ classes/genre.rb
@@ -0,0 +1,18 @@
+require_relative 'items'
+
+class Genre < items
+  attr_reader :id, :items
+  attr_accessor :name
+  
+  def initialize(name)
+    @id = rand(1..1000)
+    @name = name
+    @items = []
+    super()
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_genre = self
+  end
+end
