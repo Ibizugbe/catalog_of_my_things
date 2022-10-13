b52a32005d404b0de32221b485388e47239353ec
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-13 12:25:05 +0100

    create table for music_albums

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,8 @@
 -- create database for catalog
 CREATE DATABASE IF NOT EXISTS catalog_of_things;
 
-CREATE TABLE MyItems(
+-- create table my_catalog
+CREATE TABLE my_catalog(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
   genre_id     INT NOT NULL,
   author_id    INT NOT NULL,
@@ -11,4 +12,15 @@ CREATE TABLE MyItems(
   CONSTRAINT label_fk FOREIGN KEY (label_id) REFERENCES Labels (id),
   CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES Authors (id),
   CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES Genres (id)
+);
+
+-- create table for music_albums
+CREATE TABLE music_albums(
+  ID SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  on_spotify BOOLEAN NOT NULL,
+  archived BOOLEAN NOT NULL,
+  label_ID INT REFERENCES label(ID),
+  author_ID INT REFERENCES author(ID),
+  genre_ID INT REFERENCES genre(ID)
 );
